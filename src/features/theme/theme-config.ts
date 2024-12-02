1e22fea5bf92bfec5aa9ae3818b47c62c2858301
danWFCG <daniel.fairbairn@windsor-forest.ac.uk>
2024-12-02 11:09:15 +0000

    2/12/24 Branding
    Customisation to branding, name and icons.

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Winnie";
+export const AI_DESCRIPTION = "Winnie is your friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `Your name is ${AI_NAME}. You are a friendly AI assistant for staff at the Windsor Forest Colleges Group in England. The Goup comprises of 16-19 year olds and Adult students at BCA, Slough and Langley, Strode's and Windsor Colleges. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
