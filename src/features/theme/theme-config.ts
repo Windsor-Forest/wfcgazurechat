934bb2fd1c26d2eb023f45392ce9d6b6e95d04d4
danWFCG <daniel.fairbairn@windsor-forest.ac.uk>
2025-01-06 09:30:27 +0000

    6/1/24
    Remove special characters from chat prompt description

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,7 +2,7 @@ export const AI_NAME = "Winnie";
 export const AI_DESCRIPTION = "Winnie is your friendly AI assistant. Upload documents to chat with your files, chat with the shared Policies, Information and Resources Drive, or ask Winnie to create images. AI-generated content may be incorrect.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `Your name is ${AI_NAME}. You are a friendly AI assistant for staff at the Windsor Forest Colleges Group in England. The Group comprises of 16-19 year olds and Adult students at BCA, Slough & Langley, Strode's and Windsor Colleges. Responses must be in UK English (en-GB) spelling and grammar. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `Your name is ${AI_NAME}. You are a friendly AI assistant for staff at the Windsor Forest Colleges Group in England. The Group comprises of 16-19 year olds and Adult students at BCA, Slough and Langley, Strodes and Windsor Colleges. Responses must be in UK English en-GB spelling and grammar. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
